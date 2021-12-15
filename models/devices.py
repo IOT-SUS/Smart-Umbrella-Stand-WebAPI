357f24bf95d06398ca1142cacdf463f981290f00
maggie62755 <73820404+maggie62755@users.noreply.github.com>
2021-12-15 20:24:16 +0800

    1215 update device.py
    1215 update
    1. controllers/device.py
    2. models/device.py

--- models/devices.py
+++ models/devices.py
@@ -1,36 +1,43 @@
 import uuid
-
+ 
+from datetime import datetime
+ 
+from flask_bcrypt     import Bcrypt
 from app import app
 from app import mongo
-from datetime import datetime
-from flask_bcrypt     import Bcrypt
-
+ 
 bcrypt = Bcrypt(app)
-
+ 
 class devicesModel():
-
+ 
     @staticmethod
     def add(device):
-        now_time = datetime.now().isoformat()
         device['public_id']  = str(uuid.uuid4())[:8]
-        device['created_at'] = now_time
-        device['updated_at'] = now_time
         mongo.db.devices.insert_one(device)
-        
+        return device['public_id']
+       
     @staticmethod
     def find(device_id):
         return list(mongo.db.devices.find({'public_id': device_id}))
-
+ 
+    #find by locaiotn
+    @staticmethod
+    def find_by_loc(location):
+        return list(mongo.db.devices.find({'location': location}))
+ 
+ 
     @staticmethod
     def find_all():
         return devicesModel.devices
-
+ 
     @staticmethod
     def update(device_id, update_query):
-        now_time = datetime.now().isoformat()
-        update_query['updated_at'] = now_time
         mongo.db.devices.update_one({'public_id': device_id}, {'$set': update_query})
-    
+   
     @staticmethod
     def delete(device_id):
         mongo.db.devices.delete_one({'public_id': device_id})
+ 
+ 
+ 
+

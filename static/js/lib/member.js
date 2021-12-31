129e5a6dd41897b82ea0e331433fc14e3a89615d
Amiannn <wang.amiannn@gmail.com>
2022-01-01 03:25:24 +0800

    Add Device details information page

--- static/js/lib/member.js
+++ static/js/lib/member.js
@@ -1,8 +1,8 @@
-import { getJwtToken } from './utils.js';
-import { userApi }     from './api/user.js';
-import { rentApi }     from './api/rent.js';
-import { index }       from './index.js';
+import { getJwtToken }   from './utils.js';
+import { userApi }       from './api/user.js';
+import { index }         from './index.js';
 import { umbrellaTable } from './umbrella.js';
+import { deviceApi }     from './api/device.js';
 
 export class member {
     static checkLogined() {
@@ -58,5 +58,109 @@ recordButton.onclick = () => {
     document.getElementById("recordTable").style.display = "block";
 }
 
+var recordButton = document.getElementById("btn_device");
+recordButton.onclick = () => {
+    document.getElementById("memberFirstPage").style.display = "none";
+    document.getElementById("formMember").style.display = "none";
+    document.getElementById("recordTable").style.display = "none";
+    document.getElementById("deviceTable").style.display = "block";
+    deviceDetailTable.get()
+}
 
 
+// device details
+
+export class deviceDetailTable{
+    static get() {
+        deviceApi.infos(deviceDetailTable.checkTableSuccess, deviceDetailTable.checkTableFaild);
+    }
+
+    static update() {
+        deviceApi.infos(deviceDetailTable.updateTable, deviceDetailTable.checkTableFaild);
+    }
+
+    static updateTable(res) {
+        $('#deviceDetailTable').bootstrapTable('load',  getMyData())
+        function getMyData() {
+            var mydata = [];
+            
+            for (var i = 0; i < res.data.length; i++) {
+                var status_updated = new Date(res.data[i].status_updated_at);
+                var now_date       = Date.now()
+                var time_diff      = diff_minutes(now_date, status_updated)
+                
+                mydata.push({
+                    device_id : res.data[i].public_id,
+                    ip        : res.data[i].status[0],
+                    wifi      : res.data[i].status[1],
+                    running   : (time_diff < 0.1) ? '● Running'  : 'Stop'
+                });
+            }
+            return mydata;
+        }
+    }
+    
+    static checkTableSuccess(res) {
+        console.log(res);
+        $('#deviceDetailTable').bootstrapTable({
+            columns:[ 
+                {field:'checkbox'   , title:'checkbox'   , align:'center', width:40, visible:true, checkbox:true},
+                {field:'running'    , title:'Status'     , align:'center', width:120, visible:true, cellStyle:(value, row, index)=>deviceDetailTable.cellStyle(value, row, index)},
+                {field:'device_id'  , title:'Device ID'  , align:'center', width:80, visible:true},
+                {field:'ip'         , title:'IP Adress'  , align:'center', width:120, visible:true},
+                {field:'wifi'       , title:'Wifi'       , align:'center', width:80, visible:true},
+                ],
+                data : getMyData(),
+                search : true //查詢
+        });
+        function getMyData() {
+            var mydata = [];
+            
+            for (var i = 0; i < res.data.length; i++) {
+                var status_updated = new Date(res.data[i].status_updated_at);
+                var now_date       = Date.now()
+                var time_diff      = diff_minutes(now_date, status_updated)
+                
+                mydata.push({
+                    device_id : res.data[i].public_id,
+                    ip        : res.data[i].status[0],
+                    wifi      : res.data[i].status[1],
+                    running   : (time_diff < 0.1) ? '● Running'  : 'Stop'
+                });
+            }
+            return mydata;
+        }
+        checkDeviceTimer = setInterval(() => deviceDetailTable.update(), 500);
+    }
+
+    static checkTableFaild(err) {
+        console.log(err);
+        console.log("FAIL")
+    }
+
+    static cellStyle(value, row, index) {
+
+        if (value == "● Running") {
+            return {
+                css: {
+                    color: 'rgb(57, 230, 0)'
+                  }
+            }
+        }
+        return {
+          css: {
+            color: 'gray'
+          }
+        }
+          
+    }
+}
+
+function diff_minutes(dt2, dt1) 
+{
+
+  var diff =(dt2 - dt1) / 1000;
+  diff /= 60;
+  return Math.abs(diff);
+  
+}
